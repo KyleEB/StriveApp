1089ffefdaf131ceb852e024d1593a8a1aa7da5c
Emily Young <erae97@iastate.edu>
2019-01-27 17:29:48 -0600

    Learning Design

---
+++ Experiments/Emily's Test/Learning_Design/app/src/main/java/com/example/learning_design/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.learning_design;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b;
+    ImageButton imb;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.logout);
+        imb = findViewById(R.id.Profile);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Come back soon!!", Toast.LENGTH_LONG).show();
+            }
+        });
+        imb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
