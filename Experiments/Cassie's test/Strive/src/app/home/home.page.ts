9785f9cfa0251fd887179f3152a9ae90724be4f2
Cassie <clabath@iastate.edu>
2019-01-31 11:12:26 -0600

    Strive login/register added

---
+++ Experiments/Cassie's test/Strive/src/app/home/home.page.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router'
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  username:string;
+  password:string;
+
+  constructor(public router: Router) {
+
+  }
+
+  login(){
+    console.log("Username: "+ this.username);
+    console.log("Password: "+ this.password);
+
+  }
+
+  goRegister(){
+    this.router.navigateByUrl('/register');
+  }
+
+}
---
+++ Experiments/Cassie's test/Strive/src/app/register/register.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage{
+
+  username:string = '';
+  password:string = '';
+  repassword:string = '';
+  constructor(public router: Router) { 
+    
+  }
+
+  register(){
+    if(this.username.length==0)
+    {
+      alert("Please enter username");
+    } else if (this.password.length==0) {
+      alert("Please enter password");
+    } else if (this.repassword.length==0) {
+      alert("Please repeat password") 
+    }
+  }
+
+}
