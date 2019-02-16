1f83d7f1a97611d50f4ce0bb5551866802a866f5
Kyle <kbowden@iastate.edu>
2019-02-16 17:07:18 -0600

    Added Error Recognition to Signup

--- Backend/restAPI/routes/userRoute.js
+++ Backend/restAPI/routes/userRoute.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const userCtrl = require('../controllers/userCtrl');
 
 router.post('/signup/user', userCtrl.createUser)
+router.post('/login/user', userCtrl.loginUser);
+
 module.exports = router;
--- Frontend/Strive/src/app/register/register.page.ts
+++ Frontend/Strive/src/app/register/register.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { RegisterService } from './../services/register.service';
+import { LoadingController, AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
@@ -11,30 +12,65 @@ export class RegisterPage{
   username:string = '';
   password:string = '';
   repassword:string = '';
+
   constructor(
     public router: Router,
-    private reg: RegisterService
+    private reg: RegisterService,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController
     ) { 
   }
 
-  register(){
+ async register(){
     if(this.username.length==0)
     {
-      alert("Please enter username");
+      this.displayAlert('Invalid Input',"Please enter username");
     } else if (this.password.length==0) {
-      alert("Please enter password");
-    } else if (this.repassword.length==0) {
-      alert("Please repeat password") 
-    }
+      this.displayAlert('Invalid Input',"Please enter password");
+    } else if (this.repassword.length==0 || this.repassword != this.password) {
+      this.displayAlert('Invalid Input',"Please repeat password") 
+    } else {
 
+    await this.showLoading();
     this.reg.registerUser(this.username, this.password)
-    .subscribe(res => {
+    .subscribe(async res =>  {
+        this.loadingCtrl.dismiss();
         console.log(res)
+        
+        if(res.error){
+          this.displayAlert('Sign Up Error', res.error);
+        }
+    
+        if(res.user){
+          this.loginreturn();
+        }
       });
+    }
+      this.username = '';
+      this.password = '';
+      this.repassword = '';
   }
 
   loginreturn() {
     this.router.navigateByUrl('/home')
   }
 
+   async showLoading(){
+    const loading =  await this.loadingCtrl.create({
+      message: 'Loading...',
+      spinner: 'bubbles',
+      duration: 3000
+    });
+
+     return await loading.present();
+  }
+
+  async displayAlert(header, subheader){
+    const alert = await this.alertCtrl.create({
+      header: header,
+      subHeader: subheader,
+      buttons: ['Dismiss']
+    });
+    return await alert.present();
+  }
 }
