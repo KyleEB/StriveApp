936bf105f71ea3f8bfff8a931453eb837640e30d
Kyle <kbowden@iastate.edu>
2019-02-16 21:07:22 -0600

    Username presented at login

--- Frontend/Strive/src/app/main/main.module.ts
+++ Frontend/Strive/src/app/main/main.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule, Router } from '@angular/router';
+import { Routes, RouterModule} from '@angular/router';
 
-import { IonicModule, LoadingController, AlertController } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 
 import { MainPage } from './main.page';
-import { RegisterService } from '../services/register.service';
-import { Storage } from '@ionic/storage';
 
 const routes: Routes = [
   {
@@ -26,21 +24,4 @@ const routes: Routes = [
   declarations: [MainPage]
 })
 export class MainPageModule {
-  username:any;
-  constructor(public router: Router,
-    private reg: RegisterService,
-    private loadingCtrl: LoadingController,
-    private alertCtrl: AlertController,
-    private storage: Storage){
-      this.loadUser();
-    }
-
-    async loadUser(){
-      await this.storage.get('username').then((value) => {
-        console.log('your name is ' + value);
-        this.username = value; 
-      });
-      console.log('your username is ' + this.username);
-      
-    }
 }
--- Frontend/Strive/src/app/main/main.page.ts
+++ Frontend/Strive/src/app/main/main.page.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Router } from '@angular/router';
+import { RegisterService } from '../services/register.service';
+import { LoadingController, AlertController } from '@ionic/angular';
+
+import { Storage } from '@ionic/storage'
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.page.html',
   styleUrls: ['./main.page.scss'],
 })
-export class MainPage implements OnInit {
+export class MainPage {
 
-  constructor() { }
+    username: any;
 
-  ngOnInit() {
-  }
+    constructor(public router: Router,
+    private reg: RegisterService,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController,
+    private storage: Storage){
+      this.loadUser();
+    }
+
+    async loadUser(){
+       await this.storage.get('username').then((value) => {
+        console.log('your name is ' + value);
+        this.username = value; 
+      });
+      console.log('your username is ' + this.username);
+      
+    }
 
 }
