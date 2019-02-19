bd93a50c68e93d879f6fc5ea9763378418fc8c64
Cassie <clabath@iastate.edu>
2019-02-19 14:10:40 -0600

    basic profile framework, subscribe/subscribed button

--- Frontend/Strive/src/app/pages/profile/profile.page.ts
+++ Frontend/Strive/src/app/pages/profile/profile.page.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
+  subColor:string="primary";
+  subscribeStatus:string="Subscribe"
+  subscribed:boolean=false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  subscribeStatusChange(){
+    if(this.subscribed==false)
+    {
+      this.subscribed=true;
+      this.subscribeStatus="You are Subscribed!"
+    } else {
+      this.subscribed=false;
+      this.subscribeStatus="Subscribe"
+    }
+  }
+
+  changeColor(){
+    if(this.subColor=="primary")
+    {
+      this.subColor="light";
+    } else {
+      this.subColor="primary";
+    }
   }
 
 }
