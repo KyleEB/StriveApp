57d94b3bd171c2b25ebdb4f4d47c1c49f93d78c5
Emily Young <erae97@iastate.edu>
2019-03-07 16:14:00 -0600

    added checklist-limited functionality

--- Frontend/Strive/src/app/pages/calendar/calendar.page.ts
+++ Frontend/Strive/src/app/pages/calendar/calendar.page.ts
@@ -9,6 +9,12 @@ export class CalendarPage implements OnInit {
 
   constructor() { }
 
+  public form = [
+      { val: 'Task 1', isChecked: false },
+      { val: 'Task 2', isChecked: false },
+      { val: 'Task 3', isChecked: false }
+    ];
+  
   ngOnInit() {
   }
 
