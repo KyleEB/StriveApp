88ac1b3b6ded4ae34c3fb12447930eba54836687
Cassie <clabath@iastate.edu>
2019-03-09 17:28:42 -0600

    audio service and meditation sounds implemented

--- Frontend/Strive/src/app/pages/meditation/meditation.page.ts
+++ Frontend/Strive/src/app/pages/meditation/meditation.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { ThemeService } from 'src/app/theme.service';
+import { AudioService } from '../../services/audio.service';
 
 @Component({
   selector: 'app-meditation',
@@ -9,11 +10,22 @@ import { ThemeService } from 'src/app/theme.service';
 export class MeditationPage implements OnInit {
 
   constructor(
-    private theme: ThemeService
+    private theme: ThemeService,
+    private audio: AudioService
   ) {
     this.theme.storedTheme;
    }
   ngOnInit() {
+  }
+
+  ngAfterViewInit(){
+
+    this.audio.preload('forestSounds', '../../../assets/meditation/forest.wav');
+
+  }
+
+  play(){
+    this.audio.play('forestSounds');
   }
 
 }
