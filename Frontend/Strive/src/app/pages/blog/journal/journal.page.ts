18c9df4d310a8a363a405e23a6f5a507e4a6d34f
Emily Young <erae97@iastate.edu>
2019-03-10 14:40:59 -0500

    very basic journal features

--- Frontend/Strive/src/app/pages/blog/journal/journal.page.ts
+++ Frontend/Strive/src/app/pages/blog/journal/journal.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ThemeService } from 'src/app/theme.service';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-journal',
@@ -9,13 +10,20 @@ import { ThemeService } from 'src/app/theme.service';
 export class JournalPage implements OnInit {
 
   constructor(
-    private theme: ThemeService
+    private theme: ThemeService,
+	private storage: Storage
   ) {
     this.theme.storedTheme;
    }
    
    save(){
+	this.storage.set('newNote', this.text);
 	
+   }
+   viewNotes(){
+	 this.storage.get('newNote').then((data) => {
+		console.log(data);
+	 });
    }
   ngOnInit() {
   }
