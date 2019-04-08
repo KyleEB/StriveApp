01e9a6d02f670c690a1e87c25f5674a35e5eb159
Kyle <kbowden@iastate.edu>
2019-04-07 21:26:02 -0500

    Fixed Cards

--- Backend/restAPI/models/user.js
+++ Backend/restAPI/models/user.js
@@ -6,7 +6,7 @@ const userSchema = mongoose.Schema({
     password: {type : String},
     email: {type : String},
     fullname: {type : String},
-    cards: [{ name : String , goal : String}]
+    cards: [{ name : String , goal : String, desc : String}]
 });
 
 userSchema.methods.encryptPassword = (password) => {
