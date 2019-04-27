ce92f67f90f69035084a992eec5b2594235ebf3b
Cassie <clabath@iastate.edu>
2019-04-27 15:56:47 -0500

    partially working todo

--- Frontend/Strive/src/app/pages/calendar/task.ts
+++ Frontend/Strive/src/app/pages/calendar/task.ts
@@ -2,10 +2,12 @@ export class task {
 	eventName: any;
     timeEvent: any;
     loc: any;
+    checklist: any;
 
-	constructor(event:any, time:any, location:any){
+	constructor(event:any, time:any, location:any, checklist:any){
 		this.eventName = event;
         this.timeEvent= time;
         this.loc = location;
+        this.checklist = checklist;
 	}
 }
